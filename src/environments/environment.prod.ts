3feb7184fce92baa5be2ab080a62541c5a34175b
choukri <abdellahchoukri2.0@gmail.com>
2021-10-03 13:40:57 +0200

    add cors anywhere

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   APIbaseUrl: 'https://em-backend-spingboot.herokuapp.com/api/v1/',
-  corsPolicy: 'https://cors-anywhere.herokuapp.com',
+  corsPolicy: 'https://cors-anywhere.herokuapp.com/',
   
 };
