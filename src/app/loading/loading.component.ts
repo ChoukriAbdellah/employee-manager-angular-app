2dacf359f64726988973759b9e9e9cbd58e857c2
choukri <abdellahchoukri2.0@gmail.com>
2021-10-17 12:39:04 +0200

    add loading component

---
+++ src/app/loading/loading.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'loading-component',
+    templateUrl: 'loading.component.html',
+    styleUrls: ['loading.style.css']
+})
+
+export class LoadingComponent  implements OnInit {
+
+    ngOnInit(): void {
+
+    }
+}
